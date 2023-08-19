e3812ad1ff568051ecd875ee1424ddee2d07fc5b
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-08-19 18:06:33 +0500

    fixed email joi

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(requestLogger);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
