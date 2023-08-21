159cac13dd3f0a9a20379c123d80f4ecfb3a6525
Sergey Pozdnyakov <mislikr45@gmail.com>
2023-08-21 19:47:12 +0500

    4

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -14,11 +14,11 @@ function Card({
    
   const isOwn = card.owner === currentUser._id;
   const isLiked = card.likes.some(i => i === currentUser._id);
-  const cardLikeButtonClassName = `card-item__like {isActive ? "card-item__like_selected" : ' '}`;
+  const cardLikeButtonClassName = `card-item__like {isLiked ? "card-item__like_selected" : ' '}`;
   
   
   console.log(isLiked, currentUser._id)
-  // function likeremadd() {
+
   // const cardLikeButtonClassName = `card-item__like ${
   //   isLiked && "card-item__like_selected"
   // }`;
